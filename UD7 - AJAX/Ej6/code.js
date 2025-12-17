1124c2c8b81a92b75f58291a37793602a93718a2
Javier <javmanfer@gmail.com>
2025-12-17 22:09:53 +0100

    Ejercicio 6

--- UD7 - AJAX/Ej6/code.js
+++ UD7 - AJAX/Ej6/code.js
@@ -1,15 +1,107 @@
 "use strict";
 
-const cargarUsuarios = async () =>{
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
 
+
+const crearDialog = (mensaje, tipo)=>{
+  const dialog = document.createElement("dialog");
+  
+  const h2 = document.createElement("h2");
+  if(tipo === DIALOGO_ERROR) 
+    h2.textContent = "Error";
+  else
+    h2.textContent = "Resultado";
+
+  const button = document.createElement("button");
+  button.textContent = "Cerrar";
+  button.addEventListener("click", e=>{
+    dialog.cerrar;
+  })
+
+  dialog.append(h2, mensaje, button);
+  document.body.append(dialog);
 }
+
+
+
+
+const cargarUsuarios = async () =>{
+  try{
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    
+    if(!response.ok)
+      throw new Error("No se pudieron recuperar los usuarios");
+
+    const json = await response.json();
+    tratarJSONUsuarios(json);
+  }
+  catch(error){
+    crearDialog(error, DIALOGO_ERROR);
+  }
+}
+
+
+
+const tratarJSONUsuarios = (json) =>{
+  //Si la tabla ya está creada salimos o la eliminamos
+  const tabla = document.querySelector("#tabla")
+  if(tabla) return; //tabla.remove();
+
+  // La tabla
+  const table = document.createElement("table");
+  table.id = "tabla";
+
+  //La cabecera
+  const thead = document.createElement("thead");
+  const trhead = document.createElement("tr");
+  const th1 = document.createElement("th");
+  const th2 = document.createElement("th");
+  const th3 = document.createElement("th");
+  const th4 = document.createElement("th");
+
+  th1.innerText ="Nombre";
+  th2.innerText ="E-mail";
+  th3.innerText ="Web";
+  th4.innerText ="Ciudad";
+
+  table.append(thead);
+  thead.append(trhead);
+  trhead.append(th1, th2, th3, th4);
+
+  //El tbody
+  const tbody = document.createElement("tbody");
+
+  //recorremos el array de datos añadimos una fila por cada elemento
+  for(let usuario of json){ 
+    const tr = document.createElement("tr");
+    const td1 = document.createElement("td");
+    const td2 = document.createElement("td");
+    const td3 = document.createElement("td");
+    const td4 = document.createElement("td");
+    td1.innerText = usuario.name;
+    td2.innerText = usuario.email;
+    td3.innerText = usuario.website;
+    td4.innerText = usuario.address.city;
+    tr.append(td1,td2,td3,td4);
+    tbody.append(tr);
+  }
+
+  //Añadimos el tbody a la tabla
+  table.append(tbody);
+
+  // Añadimos el evento delegado a la tabla
+  // anadirEventoDelegadoTabla(table);
+
+  // Añadimos la tabla al documento o la devolvemos
+  document.body.append(table);
+}
+
 
 
 ///////////////////////
 //MAIN
 //////////////////////
-
+const DIALOGO_ERROR = 1;
 
 // https://jsonplaceholder.typicode.com/users
 
