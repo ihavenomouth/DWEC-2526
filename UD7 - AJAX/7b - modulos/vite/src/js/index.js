921f99dcfc7cecfb98ada4736e6904c72f220a98
Javier <javmanfer@gmail.com>
2026-01-14 22:11:53 +0100

    Terminado el ejercicio de Vite

--- UD7 - AJAX/7b - modulos/vite/src/js/index.js
+++ UD7 - AJAX/7b - modulos/vite/src/js/index.js
@@ -1,7 +1,15 @@
-import { annotate } from 'rough-notation';
+// import { annotate } from 'rough-notation';
 // Or using unpkg
 // import { annotate } from 'https://unpkg.com/rough-notation?module';
 
+// const annotation = annotate(btnAccion, { type: 'underline' });
+// annotation.show();
+
 const btnAccion = document.querySelector('#btnAcción');
-const annotation = annotate(btnAccion, { type: 'underline' });
-annotation.show();
+
+btnAccion.addEventListener("click", async e=>{
+  const {annotate} = await import ('rough-notation');
+  
+  const annotation = annotate(btnAccion, { type: 'underline' });
+  annotation.show();
+})
